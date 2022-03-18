42de9f560614ec6ebbf8912b20a2192c684e780f
Andy Wagner <andyw@graphcore.ai>
2022-03-18 23:49:05 +0000

    Adding Offlien Mode

---
+++ offline/bert_model/bert_config.py
@@ -0,0 +1,51 @@
+
+from dataclasses import dataclass
+from enum import Enum, unique
+from .modeling import PipelinedBertForSequenceClassification, PipelinedBertForTokenClassification
+import ctypes
+import os
+
+        
+
+def handle_custom_ops(config):
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    CUSTOM_OP_PATH = os.path.join(file_dir, "custom_ops.so")
+    if os.path.exists(CUSTOM_OP_PATH):
+        ops_and_patterns = ctypes.cdll.LoadLibrary(CUSTOM_OP_PATH)
+        ops_and_patterns.setVocabSize(config.vocab_size)
+        ops_and_patterns.setEmbeddingSize(config.hidden_size)
+        ops_and_patterns.setHiddenSize(config.hidden_size)
+    else:
+        exit()
+
+@unique
+class BertTaskEnum(str,Enum):
+    SEQUENCE = "Sequence"
+    TOKEN = "Token"
+
+@dataclass
+class BertSpecific:
+    model_path:str="./bert_model"
+    sequence_length:int=384
+    embedding_serialization_factor:int=1
+    tuning_type:str = "Sequence"
+    num_labels:int = 3
+    #tuning_type:BertTaskEnum=BertTaskEnum.SEQUENCE
+
+    def get_model(self, model_description, config, half=True):
+        handle_custom_ops(config)
+        config.embedding_serialization_factor=self.embedding_serialization_factor
+        config.layers_per_ipu=model_description.ipu_layout.layers_per_ipu
+        config.recompute_checkpoint_every_layer=model_description.ipu_options.recompute_checkpoint_every_layer
+        config.num_labels = self.num_labels
+        print("Number Labels Inside Here", config.num_labels)
+
+
+        if self.tuning_type == "Sequence":
+            model = PipelinedBertForSequenceClassification.from_pretrained(model_description.checkpoint, config=config).half()
+            return model
+        elif self.tuning_type == "Token":
+            model = PipelinedBertForTokenClassification.from_pretrained(model_description.checkpoint, config=config).half()
+            return model
+        else:
+            print("Error")
---
+++ offline/ipu_options.py
@@ -0,0 +1,80 @@
+# Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+import poptorch
+import popart
+import popdist
+import popdist.poptorch
+import numpy as np
+import ctypes
+import os
+
+from offline_config import Ipu
+
+def constant_options(opts):
+        # PopART options
+    opts._Popart.set("subgraphCopyingStrategy", int(popart.SubgraphCopyingStrategy.JustInTime))
+    opts._Popart.set("outlineThreshold", 10.0)
+    opts._Popart.set("accumulateOuterFragmentSettings.schedule",
+                     int(popart.AccumulateOuterFragmentSchedule.OverlapMemoryOptimized))
+    opts._Popart.set("accumulateOuterFragmentSettings.excludedVirtualGraphs", ["0"])
+    opts.enableExecutableCaching('./cache_dir')
+
+    engine_options = {
+        "opt.useAutoloader": "true",
+        "target.syncReplicasIndependently": "true",
+    }
+
+    opts._Popart.set("engineOptions", engine_options)
+
+
+def get_options(options:Ipu):
+
+
+    opts = poptorch.Options()
+    constant_options(opts)
+    
+    opts.deviceIterations(options.batches_per_step)
+
+    #mem_prop = {
+    #    f'IPU{i}': model_description.ipu_layout.matmul_proportion[i]
+    #    for i in range(int(model_description.ipu_layout.ipus_per_replica))
+    #}
+    #opts.setAvailableMemoryProportion(mem_prop)
+
+    '''
+    Set ipu specific options for the model, see documentation:
+    https://docs.graphcore.ai/en/latest/
+    '''
+
+
+
+    # Numpy options
+
+
+
+    
+
+
+    #if config.executable_cache_dir:
+    #    opts.enableExecutableCaching(config.executable_cache_dir)
+
+    # Precision options
+    if options.enable_half_partials:
+        opts.Precision.setPartialsType(torch.float16)
+
+
+
+    return opts
