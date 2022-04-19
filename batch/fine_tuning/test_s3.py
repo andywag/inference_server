b6ed2352e9381f97e5f9f24bcb4649a4febecd89
Andy Wagner <andyw@graphcore.ai>
2022-04-19 16:39:02 +0000

    Removing Fine Tuning Filter

--- batch/fine_tuning/test_s3.py
+++
@@ -1,5 +0,0 @@
-import s3fs
-#fs = s3fs.S3FileSystem(anon=True,key="AKIA5FUF7WFNKLJ5XF7L", secret="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")
-fs = s3fs.S3FileSystem(anon=True)
-
-fs.ls('graphcore-ce/andyw')
